27dc8ad7435dc4bb32e0423b5dc5659b0a5c5db1
ethanb1996 <bonanethan@gmail.com>
2024-07-12 13:27:47 +0300

    handle click interaction with the moveMade event

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, switchMap, tap } from 'rxjs';
 import { io } from 'socket.io-client';
 import { GameService } from 'src/app/services/game.service';
+import { GameStoreService } from 'src/app/store/game-store.service';
 
 @Component({
   selector: 'app-board',
@@ -9,10 +11,11 @@ import { GameService } from 'src/app/services/game.service';
 })
 export class BoardComponent implements OnInit {
   private socket: any;
+  private subscription!: Subscription;
 
   gameState: string[][] = [];
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService, private gameStore: GameStoreService) { }
 
   ngOnInit(): void {    
     this.socket = io('http://localhost:3000');
@@ -21,13 +24,35 @@ export class BoardComponent implements OnInit {
       this.gameState = gameState;
     });
 
+    this.socket.on('moveMade', (data: { gameState: string[][], winner: string | null }) => {
+      this.gameState = data.gameState;
+      if (data.winner) {
+        alert(`${data.winner} wins!`);
+      }
+      this.subscription.unsubscribe();
+    });
     this.initGame();
   }
 
   initGame(): void {
     this.gameService.initGame().subscribe(gameState => {
       this.gameState = gameState;
+      this.gameStore.resetPlayer();
     });
+  }
+
+  makeMove(row: number, col: number): void {
+    this.subscription = this.gameStore.currentPlayer$.pipe(
+      switchMap(player=>  this.gameService.makeMove(row,col,player)),
+      tap(_ => this.gameStore.togglePlayer())
+    ).subscribe()
+    // this.gameStore.currentPlayer$.subscribe(player => {
+    //   this.gameService.makeMove(row, col, player).subscribe(response => {
+    //     this.gameStore.togglePlayer();
+    //   }, error => {
+    //     console.error('Invalid move');
+    //   });
+    // }).unsubscribe(); // To avoid memory leaks
   }
   
 }
