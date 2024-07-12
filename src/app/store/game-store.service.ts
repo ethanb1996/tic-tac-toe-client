16aa36f413dd1095bcfa70e781dd5a137565522b
ethanb1996 <bonanethan@gmail.com>
2024-07-12 13:02:52 +0300

    keep the current player in the store

--- src/app/store/game-store.service.ts
+++ src/app/store/game-store.service.ts
@@ -1,9 +1,20 @@
+import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameStoreService {
+  private currentPlayerSubject: BehaviorSubject<string> = new BehaviorSubject<string>('X');
+  
+  currentPlayer$ = this.currentPlayerSubject.asObservable();
 
-  constructor() { }
+  togglePlayer(): void {
+    const nextPlayer = this.currentPlayerSubject.value === 'X' ? 'O' : 'X';
+    this.currentPlayerSubject.next(nextPlayer);
+  }
+
+  resetPlayer(): void {
+    this.currentPlayerSubject.next('X');
+  }
 }
