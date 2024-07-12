8af983d9129140641bb10591a6279ff054a315eb
ethanb1996 <bonanethan@gmail.com>
2024-07-12 12:39:57 +0300

    define route of the api into the client

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private baseUrl = 'http://localhost:3000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  initGame(): Observable<string[][]> {
+    return this.http.get<string[][]>(`${this.baseUrl}/init`);
+  }
+
+  makeMove(row: number, col: number, player: string): Observable<{ gameState: string[][], winner: string | null }> {
+    return this.http.post<{ gameState: string[][], winner: string | null }>(`${this.baseUrl}/move`, { row, col, player });
+  }
+
+  resetGame(): Observable<string[][]> {
+    return this.http.post<string[][]>(`${this.baseUrl}/reset`, {});
+  }
 }
